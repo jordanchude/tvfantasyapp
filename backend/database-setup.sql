94d949243150e2ba8d71c2755c3ae53e7da49b60
Jordan <jordan@chude.co>
2022-05-03 19:43:17 -0400

    app_user setup

---
+++ backend/database-setup.sql
@@ -0,0 +1,9 @@
+-- APP_USER
+CREATE TABLE app_user (
+  APP_USER INT NOT NULL,
+  FIRST_NAME varchar(30) NOT NULL,
+  LAST_NAME varchar(30),
+  USERNAME varchar(255),
+  EMAIL varchar(320),
+  PASSWORD varchar(255)
+);
